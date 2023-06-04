cc62ef8e2feda2fcfd76138bd18c016214d769cf
BULLSClub <117903740+BULLSClub@users.noreply.github.com>
2023-06-04 16:11:51 -0400

    Add files via upload

--- engine/configuration.js
+++ engine/configuration.js
@@ -13,23 +13,6 @@ IPFS API DETAILS
 import { create as ipfsHttpClient } from 'ipfs-http-client';
 export const client = ipfsHttpClient('https://ipfs.infura.io:5001/api/v0');
 
-/*
-HardHat Testnet
-*/
-export var hhresell = "YOUR CONTRACT ADDRESS";
-export var hhnftcol = "YOUR CONTRACT ADDRESS";
-export var hhnft = "YOUR CONTRACT ADDRESS";
-export var hhmarket = "YOUR CONTRACT ADDRESS";
-export var hhrpc = "http://localhost:8545";
-
-/*
-Goerli Testnet
-*/
-export var goeresell = "YOUR CONTRACT ADDRESS";
-export var goenftcol = "YOUR CONTRACT ADDRESS";
-export var goenft = "YOUR CONTRACT ADDRESS";
-export var goemarket = "YOUR CONTRACT ADDRESS";
-export var goerpc = "https://rpc.ankr.com/eth_goerli";
 
 /*
 BSC Testnet
@@ -37,31 +20,34 @@ BSC Testnet
 export var bsctresell = "YOUR CONTRACT ADDRESS";
 export var bsctnftcol = "YOUR CONTRACT ADDRESS";
 export var bsctnft = "YOUR CONTRACT ADDRESS";
+
+
+
 export var bsctrpc = "https://bsc-dataseed1.ninicoin.io";
 export var polygonRpc = "https://polygon-rpc.com/";
 
-/*
-Mumbai Testnet
-*/
-export var mmresell = "YOUR CONTRACT ADDRESS";
-export var mmnftcol = "YOUR CONTRACT ADDRESS";
-export var mmnft = "YOUR CONTRACT ADDRESS";
-export var mmmarket = "YOUR CONTRACT ADDRESS";
-export var mmrpc = "https://matic-testnet-archive-rpc.bwarelabs.com";
 
 
 
+export var BearNFT = "0x4C52548145a99EAcDe86561ef30Cd01c103cB579";
+export var  CowsNFT = "0xFc6D74b6c444b00f39544152553b306C24542d94";
+export var  YellowCowsNFT = "0x899310114B573E768747ea866c0edC51F39C15Cb";
+export var PolarBearNFT = "0xd2921CF90fEE0cC6Ef9961cBf60C8Cb6F07166eD";
+export var  BullsNFT = "0xdd1147C1362DcbE00f87751eeDD60cD625A4e1bA";
 
-/*
-polygon 
-*/
 
-export var cryptoBearNFT = "0x4C52548145a99EAcDe86561ef30Cd01c103cB579";
-export var  CryptoCowsClub07 = "0xFc6D74b6c444b00f39544152553b306C24542d94";
-export var  marketplaceAddress = "0x30Eff4daCc828A916438Aab8c4005B4EA2b241EE";
-export var  bsctmarket = "0x238d94ed5780f5F058a2f6CfaF3aD975887a7308";
+export var  marketplaceAddress = "0x803927bCc14A38029a6Cf1149be71cc17F12B931";
+export var  bsctmarket = "0xE2A39dF45d56A436934D8EaCbcF8465E16221f6e";
+
+
 
 export var DefaultNFTPolygon = "0xe147022fe4934f5a2Db8586196794324C62C7592";
+export var DefaultNFTBSC = "0x087EaD8aaB7488459f087508C613F53C3c1B471E";
+
+
+
+
+
 
 export var NEXT_PUBLIC_SUPABASE_URL= "https://aiymggmhhqrbszawrbyh.supabase.co"; // paste your project url here
 export var NEXT_PUBLIC_SUPABASE_ANON_KEY= "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFpeW1nZ21oaHFyYnN6YXdyYnloIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODEyMjk1NjQsImV4cCI6MTk5NjgwNTU2NH0.EcqE906g0NrF8SMJ245pGp4M-VWspL1IdTLrNH5SLIk"// paste your supabase anon key here
