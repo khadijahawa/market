8d547e3e0ceb8174a1b0b0c7a35e54596bf75e15
Khadija Hawa <khadijahawa555@gmail.com>
2023-09-06 22:47:42 +0300

    Updates

---
+++ _next/static/chunks/pages/SearchPage-648a6fa97d781090.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[526],{3852:function(n,u,_){(window.__NEXT_P=window.__NEXT_P||[]).push(["/SearchPage",function(){return _(21986)}])},21986:function(n,u,_){"use strict";_.r(u),_(67294),u.default=()=>{}}},function(n){n.O(0,[9774,2888,179],function(){return n(n.s=3852)}),_N_E=n.O()}]);
